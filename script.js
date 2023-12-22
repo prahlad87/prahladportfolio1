73774ef1ece00ed6e40cf3b43fa82d3959f5b14b
prahlad87 <56084316+prahlad87@users.noreply.github.com>
2023-12-23 02:15:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Full-Stack-Developer",  "Competitive-Programmer", "Problem-Solver", ],
+        strings: ["Product Developer", "Full-Stack-Developer",  "Competitive-Programmer", "Problem-Solver", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
